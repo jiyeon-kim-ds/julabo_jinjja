5fafb9d239f5374905b14b8b3680a936d6ba4564
knuckles6974 <knuckles6974@gmail.com>
2022-02-11 08:35:50 +0900

    [add]5

--- categories/urls.py
+++ categories/urls.py
@@ -5,6 +5,6 @@ from .views      import CategoryView, SubCategoryView, TypeView
 urlpatterns = {
   path('category', CategoryView.as_view()),
   path('subcategory/<int:category_id>/<int:subcategory_id>', SubCategoryView.as_view()),
-  path('type', TypeView.as_view())
+  path('type', TypeView.as_view())gr
   # path('csv', CsvView.as_view())
 }
